06cc4156492dcbcc7f1931570787896cc7350e4b
FlambergTomsk <flamberg@list.ru>
2021-10-20 15:19:20 +0700

    Init

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     base: '/site/',
   },
   head: {
-    title: 'Resume',
+    title: 'Site',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
